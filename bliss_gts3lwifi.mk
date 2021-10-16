756bf23525ce5e3035d28fb1e9d5464d26389d09
srgrusso <srgrusso240@gmail.com>
2021-10-15 20:19:22 -0500

    Blissify

--- bliss_gts3lwifi.mk
+++ bliss_gts3lwifi.mk
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from gts3lwifi device
 $(call inherit-product, device/samsung/gts3lwifi/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gts3lwifi
-PRODUCT_NAME := lineage_gts3lwifi
+PRODUCT_NAME := bliss_gts3lwifi
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T820
 PRODUCT_MANUFACTURER := samsung
