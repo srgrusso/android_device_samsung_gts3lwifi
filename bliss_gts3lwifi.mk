260b086a4ac538e5e912d91257629e3db46f2c05
srgrusso <srgrusso240@gmail.com>
2021-11-01 21:50:27 -0500

    gts3lwifi: Add Camera2

--- bliss_gts3lwifi.mk
+++ bliss_gts3lwifi.mk
@@ -27,6 +27,10 @@ $(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1440
 
+#Apps
+PRODUCT_PACKAGES += \
+    Camera2
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gts3lwifi
 PRODUCT_NAME := bliss_gts3lwifi
